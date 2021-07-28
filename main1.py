be04c0b6a44e0aa74143a1a53d671dcceb7b97e4
marathamz <hamar82@gmail.com>
2021-07-28 16:07:29 +0300

    testing

---
+++ main1.py
@@ -0,0 +1,11 @@
+year = '2021'
+print(year)
+
+age = '35'
+print(age)
+
+name = input('Как тебя зовут?: ')
+print('Привет,',name)
+
+age = int(input('Сколько Вам лет?: '))
+print('Хорошо выглядите!')
