44f7a1a697850ca315833f9492e4358eb2d85714
Marat <hamar82@gmail.com>
2021-08-03 14:31:08 +0300

    newHW_3

--- First/Lesson-3/Lesson3-2.py
+++ First/Lesson-3/Lesson3-2.py
@@ -1,18 +1,26 @@
 
-"""1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
-Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+"""2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
+ имя, фамилия, год рождения, город проживания, email, телефон.
+ Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой
  """
 
-def func(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return "Ошибка деления"
 
-a = int(input("Введите число a: "))
-b = int(input("Введите число b: "))
+def main(**kwargs):
+     return f"{kwargs['first_name']} {kwargs['last_name']} ({kwargs['year']}), {kwargs['city']}, {kwargs['phone']}, {kwargs['email']}"
 
-print(func(a, b))
+
+user_name = input("Имя: ")
+user_last_name = input("Фамилия: ")
+user_year = int(input("Год рождения: "))
+user_city = input("Город: ")
+user_email = input("email: ")
+user_phone = input("Номер телефона: ")
+
+print(
+    main(first_name=user_name, last_name=user_last_name,
+         year=user_year, city=user_city, email=user_email,
+         phone=user_phone)
+)
 
 
 
